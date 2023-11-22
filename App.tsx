bbd0f9084c464be0fb9ba97fd202047972f15884
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-11-22 10:19:14 -0300

    feat: configurando tema com styled-components

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
 
 export default function App() {
   return (
---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    "primary-dark": "#639339",
+    "primary-mid": "#CBE4B4",
+    "primary-light": "#E5F0DB",
+
+    "secondary-dark": "#BF3B44",
+    "secondary-mid": "#F3BABD",
+    "secondary-light": "#F4E6E7",
+
+    "gray-1": "#1B1D1E",
+    "gray-2": "#333638",
+    "gray-3": "#5C6265",
+    "gray-4": "#B9BBBC",
+    "gray-5": "#DDDEDF",
+    "gray-6": "#EFF0F0",
+    "gray-7": "#FAFAFA",
+
+    white: "#FFFFFF",
+  },
+  FONT_FAMILY: {
+    REGULAR: "NunitoSans_400Regular",
+    BOLD: "NunitoSans_700Bold",
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  },
+};
